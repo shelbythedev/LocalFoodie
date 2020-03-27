dbc6b8c65f95381c3352adb12a5a89ccaafdc3cb
Shelby Solomon <shelby@shelbysolomon.net>
2020-03-26 20:26:28 -0500

    added activeadmin, devise

---
+++ app/models/admin_user.rb
@@ -0,0 +1,8 @@
+class AdminUser < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :trackable,
+         :recoverable, :rememberable, :validatable
+
+  belongs_to :business
+end
--- app/models/business.rb
+++ app/models/business.rb
@@ -1,3 +1,3 @@
 class Business < ApplicationRecord
-
+    has_many :admin_users
 end
